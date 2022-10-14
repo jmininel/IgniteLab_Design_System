d195223eff2cd059b9cce42960bd041056adb3d4
jmininel <ju.mininel@hotmail.com>
2022-10-14 13:46:43 -0300

    Update Buttom SB Component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,8 +11,8 @@ export function Button({ children, asChild }: ButtonProps) {
    const Comp = asChild ? Slot : 'button'
 
     return (
-        <Comp className={clsx('text-gray-100 font-sans',
-                
+        <Comp className={clsx(
+          'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 transition-colors focus:ring-2 ring-white ',      
         )}>{children}</Comp>
     )
 }
