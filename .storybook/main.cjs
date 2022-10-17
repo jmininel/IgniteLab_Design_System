1c0f8883ae4181827efcfd6b43a86b8274edf265
jmininel <ju.mininel@hotmail.com>
2022-10-17 13:35:53 -0300

    ci: Add publish workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/IgniteLab_Design_System/'
+    }
+    
+    return config
   }
 }
